d07a260d0684a06bdc9438255f7b895f0638dbf9
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-02-13 00:22:24 +0530

    updated  app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+ //method one of writing code
  const deg=6;
 
  const hr=document.querySelector("#hr");
  const mn=document.querySelector("#mn");
  const sc=document.querySelector("#sc");
 
- let day= new Date();
- let hh = day.getHours() * 30;
- let mm = day.getMinutes() * deg;
- let ss = day.getSeconds() * deg;
+ setInterval(()=>{
+    let day= new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+   
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)` 
+    mn.style.transform = `rotateZ(${mm}deg)`
+    sc.style.transform = `rotateZ(${ss}deg)`
+   
+ })
 
- hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`  // this
